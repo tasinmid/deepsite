10b8f93eb1d7d17450fe8c3aab89c7feb3ec498c
tasinmid <tasinmid@gmail.com>
2025-06-12 05:19:51 +0600

    Update consts.ts

--- deepsite/utils/consts.ts
+++ deepsite/utils/consts.ts
@@ -10,8 +10,8 @@ export const defaultHTML = `<!DOCTYPE html>
     <div class="w-full">
       <span class="text-xs rounded-full mb-2 inline-block px-2 py-1 border border-amber-500/15 bg-amber-500/15 text-amber-500">🔥 New version dropped!</span>
       <h1 class="text-4xl lg:text-6xl font-bold font-sans">
-        <span class="text-2xl lg:text-4xl text-gray-400 block font-medium">I'm ready to work,</span>
-        Ask me anything.
+        <span class="text-2xl lg:text-4xl text-gray-400 block font-medium">I'm ready to get fucked,</span>
+        Fuck me Daddy.
       </h1>
     </div>
       <img src="https://enzostvs-deepsite.hf.space/arrow.svg" class="absolute bottom-8 left-0 w-[100px] transform rotate-[30deg]" />
